87557462f099bc92caf8c1591987e21e4f05c3ad
elifelse <elifgsavukcam@gmail.com>
2021-10-15 13:01:43 +0300

    list notes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Button, button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+
+const apiRoot = "https://localhost:44372/api/Notes"
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetch(apiRoot)
+      .then(response => response.json())
+      .then(data => setNotes(data));
+  }, []);
+
   return (
     <div className="container mt-3">
       <h1>RNote</h1>
-      <button className="btn btn-primary">Bir Buton</button>
-      <Button variant="danger">Bir Buton</Button>
+    <ul>
+      {notes.map(x => <li>{x.title}</li>)}
+    </ul>
     </div>
   );
 }
